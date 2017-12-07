f6c4bd9abaf0a7d01206ae8d57a8ebfbcfa791b6
superinka <thesuperinka@gmail.com>
2017-12-07 13:58:35 +0700

    xóa các file HMLT trong assets

--- mycrm/server.js
+++ mycrm/server.js
@@ -3,47 +3,95 @@ var app = express();
 var passport = require('passport');
 var session = require('express-session');
 var bodyParser = require('body-parser');
-
 var env = require('dotenv').load();
 var exphbs = require('express-handlebars');
+
 
 var path = require('path');
 // Serve static assets from the public folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', function(req, res) {
-    res.send('welcome to Passport with Sequelize');
-});
 
 //For BodyParser
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json);
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
 
-//For Passport
-app.use(session({ secret: 'keyboard cat', resave: true, saveUninitialized: true })); //session secret
-app.use(passport.initialize());
-app.use(passport.session());
+
+// For Passport
+// app.use(session({
+//     secret: 'keyboard cat',
+//     resave: true,
+//     saveUninitialized: true
+// })); // session secret
+// app.use(passport.initialize());
+// app.use(passport.session()); // persistent login sessions
+
 
 //For Handlebars
 app.set('views', './app/views')
 app.engine('hbs', exphbs({
-    extname: '.hbs'
+    extname: 'hbs',
+    defaultLayout: 'main',
+    layoutsDir: __dirname + '/app/views/layouts/',
+    partialsDir: __dirname + '/app/views/partials/'
 }));
+
+// //For Handlebars
+// app.set('views', './app/views')
+// app.engine('hbs', exphbs({
+//     extname: '.hbs'
+// }));
 app.set('view engine', '.hbs');
+
+app.get('/', function(req, res) {
+    res.render('home');
+});
+
+app.use(function(req, res) {
+    res.status(404);
+    res.render('404');
+});
+
+app.get('/', function(req, res) {
+
+    res.send('Welcome to Passport with Sequelize');
+
+});
 
 //Models
 var models = require("./app/models");
 
+//Routes
+var authRoute = require('./app/routes/auth.js')(app);
+
+
+// //load passport strategies
+
+// require('./app/config/passport/passport.js')(passport, models.user);
+
+
 //Sync Database
+
 models.sequelize.sync().then(function() {
+
     console.log('Nice! Database looks fine')
+
+
 }).catch(function(err) {
-    console.log(err, "Something went wrong with the Database update")
+
+    console.log(err, "Something went wrong with the Database Update!")
+
 });
 
-//set up port website
+
 app.listen(5000, function(err) {
+
     if (!err)
+
         console.log("Site is live");
+
     else console.log(err)
+
 });
